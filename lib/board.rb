4faf76db6b51afe8cdaa8329149cc30597d70f2d
SANPAK Ng <sanpakng@SANPAKs-Air.fios-router.home>
2017-06-01 18:14:38 -0400

    coding win moves

--- lib/board.rb
+++ lib/board.rb
@@ -11,10 +11,24 @@ class Board
   end
 
   def empty?(pos)
-    return true unless grid[pos[0]][pos[1]] 
+    return true unless grid[pos[0]][pos[1]]
   end
 
   def winner
+    win_moves = [
+      [[0,0],[0,1],[0,2]], #top row
+      [[1,0],[1,1],[1,2]],
+      [[2,0],[2,1],[2,2]],
+      [[0,0],[1,1],[2,2]],
+      [[0,1],[1,1],[2,1]],
+      [[0,2],[1,1],[2,0]]
+    ]
+
+    win_moves.each do |combination|
+      return :X if combination.all? { |el| grid[el[0]][el[1]] == :X }
+      return :O if combination.all? { |el| grid[el[0]][el[1]] == :O }
+    end
+    return nil
   end
 
   def grid
