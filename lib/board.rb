3541614c79a1e02677ef115658a279ff47a9c025
SANPAK Ng <sanpakng@SANPAKs-Air.fios-router.home>
2017-06-01 17:35:10 -0400

    trying to code winner

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,26 @@
 class Board
+  attr_reader :grid
+
+  def initialize(grid = nil)
+    grid = [[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]] if grid == nil
+    @grid = grid
+  end
+
+  def place_mark(pos,mark)
+    grid[pos[0]][pos[1]] = mark
+  end
+
+  def empty?(pos)
+    return true unless grid[pos[0]][pos[1]] 
+  end
+
+  def winner
+  end
+
+  def grid
+    @grid
+  end
+
+  def over?
+  end
 end
