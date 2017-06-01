515603b749329022ce90de69ba3be9a02eb4a434
SANPAK Ng <sanpakng@SANPAKs-Air.fios-router.home>
2017-06-01 19:26:47 -0400

    solved human_player

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,2 +1,16 @@
 class HumanPlayer
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+
+  def get_move
+    puts "where"
+    move = gets.chomp.split(",")
+    move = move.map { |el| el.to_i }
+  end
+
+  def display(board)
+    puts "#{board.grid}"
+  end
 end
