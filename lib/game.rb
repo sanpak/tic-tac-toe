2bb0f8fb1ee79d6815b44b9342bb09dc5e42b281
SANPAK Ng <sanpakng@SANPAKs-MacBook-Air.local>
2017-06-03 18:39:38 -0400

    done

--- lib/game.rb
+++ lib/game.rb
@@ -3,22 +3,23 @@ require_relative 'human_player'
 require_relative 'computer_player'
 
 class Game
-  attr_reader :board, :player_one, :player_two
-  def initialize(player_one,player_two)
+  attr_accessor :board, :player_one, :player_two, :current_player
+  def initialize(player_one = HumanPlayer.new,player_two = ComputerPlayer.new)
     @player_one = player_one
     @player_two = player_two
-  end
-
-  def board
-    board = Board.new
-    board.grid 
+    @board = Board.new
+    @current_player = player_one
   end
 
   def play_turn
-
+    move = @current_player.get_move
+    board.place_mark(move,:X)
+    switch_players!
   end
 
-  def current_player
+
+  def switch_players!
+    @current_player = player_two
   end
 
 end
